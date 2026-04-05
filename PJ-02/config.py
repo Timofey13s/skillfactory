1afb871067fff27b76d0aa1b6660611e9053c34f
Timofey13s <timofey13s@gmail.com>
2026-04-05 20:47:54 +0300

    Add files via upload

--- PJ-02/config.py
+++ PJ-02/config.py
@@ -1,1 +1,3 @@
-
+TOKEN = '8279829077:AAE9M-nqnhZfZ1OZ26bk0cs7GndQWqlUg6Q'
+name_cur = {'рубль' : 'руб.', 'доллар' : 'дол.', 'евро' : 'евро'}
+code_cur = {'рубль' : None, 'доллар' : 'USD', 'евро' : 'EUR'}
